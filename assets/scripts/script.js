5f8d29c9c2aac428d5fff807aa930f7c52130010
Jennifer Ross <jjannross@gmail.com>
2021-01-21 17:36:40 -0500

    formated date, began 5-day forecast

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,13 @@
-//TO DO:
-// 1. set up 3 API Keys to include, temp, humidity, wind speed, uv index and 5 day forcast
-//2. save searched city to local storage
-//3. append previously searched cities to right sidebar
-
-console.log("loaded");
-
 // Event listener for all button elements
 $("#citySearch").on("submit", function (event) {
   event.preventDefault();
   console.log("click registered");
 
   var city = $("#input-city").val();
-  var date = new Date();
+
   //   var today = date
   var queryURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&appid=7c6c4a90f3674b080b9c698fb64961da&units=imperial";
   // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -23,47 +16,65 @@ $("#citySearch").on("submit", function (event) {
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (currentForecast) {
-    console.log(currentForecast);
-    console.log(currentForecast.city.coord.lat);
-    console.log(currentForecast.city.coord.lon);
+  }).then(function (currentWeather) {
+    console.log(currentWeather);
+    console.log(currentWeather.coord.lat);
+    console.log(currentWeather.coord.lon);
 
-    $("#cityName").text(currentForecast.city.name + " (" + date + ")");
-    $("#temp").text(
-      "Temperature: " + currentForecast.list[0].main.temp + "° F"
-    );
-    $("#humidity").text(
-      "Humidity: " + currentForecast.list[0].main.humidity + "%"
-    );
-    $("#wind-speed").text(
-      "Wind Speed: " + currentForecast.list[0].wind.speed + " MPH"
-    );
+    var date = new Date(currentWeather.dt * 1000);
+    var finalDate =
+      date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
 
-    var lat = currentForecast.city.coord.lat;
-    var lon = currentForecast.city.coord.lon;
+    $("#cityName").text(currentWeather.name + " (" + finalDate + ")");
+    $("#temp").text("Temperature: " + currentWeather.main.temp + "° F");
+    $("#humidity").text("Humidity: " + currentWeather.main.humidity + "%");
+    $("#wind-speed").text("Wind Speed: " + currentWeather.wind.speed + " MPH");
+
+    var lat = currentWeather.coord.lat;
+    var lon = currentWeather.coord.lon;
     var oneCallURL =
       "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       lat +
       "&lon=" +
       lon +
-      "&exclude=minutely,hourly,&appid=7c6c4a90f3674b080b9c698fb64961da";
+      "&exclude=minutely,hourly&appid=7c6c4a90f3674b080b9c698fb64961da&units=imperial";
 
     //i need to get the long and lat for the one call api
     $.ajax({
       url: oneCallURL,
       method: "GET",
     }).then(function (oneCall) {
-      //   var uv = oneCall.current.uvi;
-
       $("#uv-index").text(oneCall.current.uvi);
-      //   $("#uv-index").text("UV Index: " + uv);
-      //   $(uv).attr(background - color, red);
-      $("#day1").text(oneCall.daily[0]);
-      $("#day2").text(oneCall.daily[1]);
-      $("#day3").text(oneCall.daily[2]);
-      $("#day4").text(oneCall.daily[3]);
-      $("#day5").text(oneCall.daily[4]);
-      console.log(oneCall);
+
+      var forecastDate = new Date(oneCall.daily[1].dt * 1000);
+      var finalForecastDate =
+        date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+
+      for (var i = 1; i < 6; i++) {
+        var finalForecastDate = finalForecastDate[i];
+        //   console.log(finalForecastDate);
+      }
+
+      var forecastDays = ["#day1", "#day2", "#day3", "#day4", "#day5"];
+      for (var d = 0; d < forecastDays.length; i++) {
+        var forecastDays = forecastDays[d];
+        //   console.log(forecastDays);
+      }
+
+      $(forecastDays).html(`
+      <h6>${finalForecastDate}</h6>
+      <p>Temperature: ${oneCall.daily[1].temp.day}° F</p>
+      <p>Humidity: ${oneCall.daily[1].humidity}%</p>`);
+
+      //   $("#day1").html(`
+      //   <h6>${finalForecastDate}</h6>
+      //   <p>Temperature: ${oneCall.daily[1].temp.day}° F</p>
+      //   <p>Humidity: ${oneCall.daily[1].humidity}%</p>`);
+
+      //   $("#day2").text(oneCall.daily[1]);
+      //   $("#day3").text(oneCall.daily[2]);
+      //   $("#day4").text(oneCall.daily[3]);
+      //   $("#day5").text(oneCall.daily[4]);
     });
   });
 });
