7b35ad9529244f4047609550f723eede5439e89b
Jennifer Ross <jjannross@gmail.com>
2021-01-28 18:22:59 -0500

    fixed error code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,6 @@
 // Event listener for all button elements
 $("#citySearch").on("submit", function (event) {
   event.preventDefault();
-  console.log("click registered");
 
   var city = $("#input-city").val();
 
@@ -16,10 +15,6 @@ $("#citySearch").on("submit", function (event) {
     url: queryURL,
     method: "GET",
   }).then(function (currentWeather) {
-    console.log(currentWeather);
-    console.log(currentWeather.coord.lat);
-    console.log(currentWeather.coord.lon);
-
     var date = new Date(currentWeather.dt * 1000);
     var finalDate =
       date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
@@ -42,7 +37,6 @@ $("#citySearch").on("submit", function (event) {
       url: oneCallURL,
       method: "GET",
     }).then(function (oneCall) {
-      console.log(oneCall);
       $("#uv-index").text(oneCall.current.uvi);
 
       $("#uv-index").each(function () {
@@ -77,48 +71,17 @@ $("#citySearch").on("submit", function (event) {
       }
 
       //attempt at local storage//
-      var searchedCitiesInput = $("#input-city");
+      // var searchedCitiesInput = $("#input-city");
 
-      var citySearchForm = $("#citySearch");
+      // var citySearchForm = $("#citySearch");
 
-      function storesearchedCities() {
-        var searchedCities = localStorage.getItem("searched") || "[]";
-        var parsedSearched = JSON.parse(searchedCities);
-        parsedSearched.push(searchedCitiesInput.val());
-        searchedCitiesInput.value = "";
-        localStorage.setItem("searched", JSON.stringify(parsedSearched));
-      }
-
-      storesearchedCities();
-
-      function rendersearchedCities() {
-        var searchedCities = localStorage.getItem("searched") || "[]";
-        var parsedSearched = JSON.parse(searchedCities);
-        console.log(searchedCities);
-        searchedCities.innterHTML = "";
-        // var cityList = $("#searched");
-
-        for (var i = 0; i < searchedCities.length; i++) {
-          var searchedCities = searchedCities[i];
-
-          $("#searched" + i).html(`
-          <h6>${searchedCities[i]}</h6>;`);
-        }
-        rendersearchedCities();
-      }
-     searchedCities.addEventListener("click", function(event) {
-        var element = event.target;
-      
-        // If that element is a button...
-        if (element.matches("searchBtn") === true) {
-          // // Get its data-index value and remove the todo element from the list
-          // var index = element.parentElement.getAttribute("data-index");
-          // todos.splice(index, 1);
-      
-          // Store updated todos in localStorage, re-render the list
-          storesearchedCities();
-          rendersearchedCities();
+      // function storesearchedCities() {
+      //   var searchedCities = localStorage.getItem("searched") || "[]";
+      //   var parsedSearched = JSON.parse(searchedCities);
+      //   parsedSearched.push(searchedCitiesInput.val());
+      //   searchedCitiesInput.value = "";
+      //   localStorage.setItem("searched", JSON.stringify(parsedSearched));
+      // }
     });
   });
-  
 });
